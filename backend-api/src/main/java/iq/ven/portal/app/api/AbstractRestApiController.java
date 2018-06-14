357d9fd379b92faced8f9425fd5115b42d44e7cb
veniq <veniq137@gmail.com>
2018-06-14 18:45:00 +0300

    Initial commit

---
+++ backend-api/src/main/java/iq/ven/portal/app/api/AbstractRestApiController.java
@@ -0,0 +1,9 @@
+package iq.ven.portal.app.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AbstractRestApiController {
+    protected static final Logger logger = LoggerFactory.getLogger(AbstractRestApiController.class);
+
+}
---
+++ backend-api/src/main/java/iq/ven/portal/app/api/PingResource.java
@@ -0,0 +1,22 @@
+package iq.ven.portal.app.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("peepee")
+public class PingResource {
+
+    @GetMapping(path = "/ping")
+    public ResponseEntity<Map<String, String>> ping() {
+        Map<String, String> map = new HashMap<>();
+        map.put("ping", "pong");
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+}
